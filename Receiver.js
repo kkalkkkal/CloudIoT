e428134c67c185bf727009eeefbf6353a72f1068
kkalkkkal <63850490+kkalkkkal@users.noreply.github.com>
2021-04-25 01:03:19 +0900

    Create Receiver.js
    new Receiver NodeJS program

---
+++ Receiver.js
@@ -0,0 +1,20 @@
+//mqtt 모듈 객체 생성
+var mqtt = require('mqtt');
+
+//client 객체 mqtt 연결
+var client  = mqtt.connect('mqtt://52.78.82.238'); // 52.78.82.238은 EC2 인스턴스의 퍼블릭 ip
+
+//fs(file system) 모듈 객체 생성
+var file_system = require('fs');
+
+// client 연결
+client.on('connect', function () {
+      client.subscribe('hello/file', () => {console.log('subscribe to hello/file');}); // hello/file
+});
+
+// 파일 수신
+client.on('message', function (topic, message) {
+      data = JSON.parse(message);
+      fs.writeFileSync(data.name, data.data);
+      client.end();
+});
