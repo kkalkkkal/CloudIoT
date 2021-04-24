84d949ad9c6e5c4a34148678e442b01327e1e558
kkalkkkal <63850490+kkalkkkal@users.noreply.github.com>
2021-04-25 02:14:59 +0900

    Update Receiver.js

--- Receiver.js
+++ Receiver.js
@@ -2,7 +2,7 @@
 var mqtt = require('mqtt');
 
 //client 객체 mqtt 연결
-var client  = mqtt.connect('mqtt://52.78.82.238'); // 52.78.82.238은 EC2 인스턴스의 퍼블릭 ip
+var client = mqtt.connect('mqtt://52.78.82.238'); // 52.78.82.238은 EC2 인스턴스의 퍼블릭 ip
 
 //fs(file system) 모듈 객체 생성
 var file_system = require('fs');
@@ -15,6 +15,6 @@ client.on('connect', function () {
 // 파일 수신
 client.on('message', function (topic, message) {
       data = JSON.parse(message);
-      fs.writeFileSync('LostArk2.jpg', data.data); // 'LostArk2'라는 이름으로 파일 생성
+      file_system.writeFileSync('LostArk2.jpg', data.data); // 'LostArk2'라는 이름으로 파일 생성
       client.end();
 });
