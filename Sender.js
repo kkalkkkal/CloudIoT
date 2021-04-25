eedf55ee60065f767c2686a8034f39d1f7863d0f
kkalkkkal <63850490+kkalkkkal@users.noreply.github.com>
2021-04-25 12:51:28 +0900

    Update Sender.js

--- Sender.js
+++ Sender.js
@@ -3,7 +3,7 @@ var mqtt = require('mqtt');
 
 // 클라이언트 mqtt 연결
 var client = mqtt.connect('mqtt://52.78.82.238'); // 52.78.82.238은 EC2 인스턴스의 퍼블릭 ip
-console.log('Client Connected');
+//console.log('Client Connected');
 
 //fs(file system) 모듈 객체 생성
 var file_system = require('fs');
@@ -11,8 +11,6 @@ var file_system = require('fs');
 // 파일 데이터 가져오기
 file = 'LostArk.jpg';
 data = file_system.readFileSync(file);
-
-console.log(data);
 
 // 데이터 전송용 버퍼 생성
 buf = {
@@ -26,4 +24,5 @@ client.on('connect', function() {
   client.publish('hello/file', JSON.stringify(buf));
   console.log('hello/file : Send OK');
   client.end();
+
 });
